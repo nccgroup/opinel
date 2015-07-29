0cdd1162d619a202bec6475ea08858e2c42ecb48
l01c d3v <l01cd3v@gmail.com>
2015-07-29 15:26:45 -0700

    Updated ELB connector

--- utils_ec2.py
+++ utils_ec2.py
@@ -3,11 +3,6 @@
 # Import AWS utils
 from AWSUtils.utils import *
 
-# Import third-party packages
-import boto
-from boto import ec2
-from boto.ec2 import elb
-
 
 ########################################
 ##### Helpers
@@ -23,8 +18,4 @@ def connect_ec2(key_id, secret, session_token,  region_name):
 # Connect to ELB API
 #
 def connect_elb(key_id, secret, session_token, region_name):
-    try:
-        return boto.ec2.elb.connect_to_region(region_name, aws_access_key_id = key_id, aws_secret_access_key = secret, security_token = session_token)
-    except Exception as e:
-        printException(e)
-        return None
+    return connect_service('elb', key_id, secret, session_token, region_name)
