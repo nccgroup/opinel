905f46fb73cb93b0419205125927988ecc28b129
l01c d3v <l01cd3v@gmail.com>
2015-08-04 19:55:10 -0700

    move get_bucket_location from Scout2 to opinel

--- opinel/utils_s3.py
+++ opinel/utils_s3.py
@@ -13,3 +13,10 @@ from opinel.utils import *
 #
 def connect_s3(key_id, secret, session_token, region_name = None, config = None, silent = False):
     return connect_service('S3', key_id, secret, session_token, region_name, config, silent)
+
+#
+# Get bucket location
+#
+def get_s3_bucket_location(s3_client, bucket_name):
+    location = s3_client.get_bucket_location(Bucket = bucket_name)
+    return location['LocationConstraint'] if location['LocationConstraint'] else 'us-east-1'
