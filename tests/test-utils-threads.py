bf8102d5cb0c36d05a3eadacf4287b43c9d2ab7a
l01c d3v <l01cd3v@gmail.com>
2017-06-13 06:15:07 -0700

    Test for threading code

--- tests/test-utils-threads.py
+++ tests/test-utils-threads.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 
+from opinel.utils.threads import *
 
 class TestOpinelUtilsThreads:
 
+    def callback(self, q, params):
+        while True:
+            try:
+                i = q.get()
+            except:
+                pass
+            finally:
+                q.task_done()
+
+
     def test_thread_work(self):
-        pass
+        targets = []
+        for i in range(50):
+            targets.append(i)
+        thread_work(targets, self.callback, {}, 10)
+        for i in range(5):
+            targets.append(i)
+        thread_work(targets, self.callback)
+
 
     def test_threaded_per_region(self):
-        pass
-
+        regions = [ 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2' ]
+        thread_work(regions, threaded_per_region, params = {'method': self.callback})
