c5df41a0240924d57e45e9f359147af783960594
l01c d3v <l01cd3v@gmail.com>
2017-06-23 16:35:33 -0700

    Add python version in username to make it work in Travis

--- tests/test-services-iam.py
+++ tests/test-services-iam.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import copy
+import sys
 import time
 
 from opinel.services.iam import *
@@ -17,29 +18,33 @@ class TestOpinelServicesIAM:
         if self.creds['AccessKeyId'] == None:
             self.creds = read_creds('travislike')
         self.api_client = connect_service('iam', self.creds)
+        self.python = re.sub(r'\W+', '', sys.version)
+
+    def make_username(self, username):
+        return '%s-%s' % (username, self.python)
 
 
     #
     # Must be first
     #
     def test_001_create_user(self):
-        user_data = create_user(self.api_client, 'OpinelUnitTest001')
+        user_data = create_user(self.api_client, self.make_username('OpinelUnitTest001'))
         assert len(user_data['errors']) == 0
-        user_data = create_user(self.api_client, 'OpinelUnitTest001')
+        user_data = create_user(self.api_client, self.make_username('OpinelUnitTest001'))
         assert len(user_data['errors']) == 1
-        user_data = create_user(self.api_client, 'OpinelUnitTest002', 'BlockedUsers')
+        user_data = create_user(self.api_client, self.make_username('OpinelUnitTest002'), 'BlockedUsers')
         assert len(user_data['errors']) == 0
-        user_data = create_user(self.api_client, 'OpinelUnitTest003', ['BlockedUsers', 'AllUsers'])
+        user_data = create_user(self.api_client, self.make_username('OpinelUnitTest003'), ['BlockedUsers', 'AllUsers'])
         assert len(user_data['errors']) == 1
-        user_data = create_user(self.api_client, 'OpinelUnitTest004', with_password = True)
+        user_data = create_user(self.api_client, self.make_username('OpinelUnitTest004'), with_password = True)
         assert len(user_data['errors']) == 0
         assert 'password' in user_data
         assert len(user_data['password']) == 16
-        user_data = create_user(self.api_client, 'OpinelUnitTest005', with_password=True ,require_password_reset = True)
+        user_data = create_user(self.api_client, self.make_username('OpinelUnitTest005'), with_password=True ,require_password_reset = True)
         assert len(user_data['errors']) == 0
         assert 'password' in user_data
         assert len(user_data['password']) == 16
-        user_data = create_user(self.api_client, 'OpinelUnitTest006', with_access_key = True)
+        user_data = create_user(self.api_client, self.make_username('OpinelUnitTest006'), with_access_key = True)
         assert len(user_data['errors']) == 0
         assert 'AccessKeyId' in user_data
         assert user_data['AccessKeyId'].startswith('AKIA')
@@ -47,10 +52,10 @@ class TestOpinelServicesIAM:
 
 
     def test_002_add_user_to_group(self):
-        create_user(self.api_client, 'OpinelUnitTest010')
-        create_user(self.api_client, 'OpinelUnitTest011')
-        add_user_to_group(self.api_client, 'OpinelUnitTest010', 'BlockedUsers', True)
-        add_user_to_group(self.api_client, 'OpinelUnitTest011', 'BlockedUsers', False)
+        create_user(self.api_client, self.make_username('OpinelUnitTest010'))
+        create_user(self.api_client, self.make_username('OpinelUnitTest011'))
+        add_user_to_group(self.api_client, self.make_username('OpinelUnitTest010'), 'BlockedUsers', True)
+        add_user_to_group(self.api_client, self.make_username('OpinelUnitTest011'), 'BlockedUsers', False)
 
 
     def test_003_delete_virtual_mfa_device(self):
@@ -59,13 +64,13 @@ class TestOpinelServicesIAM:
 
 
     def test_004_get_access_keys(self):
-        create_user(self.api_client, 'OpinelUnitTest020', with_access_key = True)
-        access_keys = get_access_keys(self.api_client, 'OpinelUnitTest020')
+        create_user(self.api_client, self.make_username('OpinelUnitTest020'), with_access_key = True)
+        access_keys = get_access_keys(self.api_client, self.make_username('OpinelUnitTest020'))
         assert len(access_keys) == 1
 
 
     def test_005_show_access_keys(self):
-        show_access_keys(self.api_client, 'OpinelUnitTest020')
+        show_access_keys(self.api_client, self.make_username('OpinelUnitTest020'))
 
 
     def test_006_init_iam_group_category_regex(self):
@@ -84,7 +89,7 @@ class TestOpinelServicesIAM:
             unmodifiable_entity = False
             remaining_users = []
             for user in users:
-                errors = delete_user(self.api_client, user)
+                errors = delete_user(self.api_client, self.make_username(user))
                 if len(errors):
                     remaining_users.append(user)
                     for handled_code in ['EntityTemporarilyUnmodifiable', 'DeleteConflict']:
