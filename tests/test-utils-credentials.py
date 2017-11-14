7e5ef8f3d4513aa05f4ad5a03d9c5238b38a065f
l01c d3v <l01cd3v@gmail.com>
2017-11-13 22:56:04 -0800

    Test one more statement

--- tests/test-utils-credentials.py
+++ tests/test-utils-credentials.py
@@ -86,10 +86,12 @@ class TestOpinelCredentialsClass:
             assert credentials['SecretAccessKey'] == result[1]
             assert credentials['SerialNumber'] == result[2]
             assert credentials['SessionToken'] == result[3]
-#        if os.path.exists(aws_config_dir):
-#            shutil.move(aws_config_dir, 'tmp%s' % aws_config_dir)
+        tmp_aws_config_dir = 'tmp_aws_config_dir'
+        if os.path.exists(aws_config_dir):
+            shutil.move(aws_config_dir, tmp_aws_config_dir)
         credentials = read_creds_from_aws_credentials_file(profile_name = 'default')
-#        shutil.move('tmp%s' % aws_config_dir, aws_config_dir)
+        if os.path.exists(tmp_aws_config_dir):
+            shutil.move(tmp_aws_config_dir, aws_config_dir)
 
 
     def test_read_creds_from_csv(self):
